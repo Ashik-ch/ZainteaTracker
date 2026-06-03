2b489584aff5a32f49769667c7edab794d080df5
Ashik-ch <ashikch7117@gmail.com>
2026-06-03 14:50:47 +0400

    package

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages is hosted at: https://ashik-ch.github.io/ZainteaTracker/
+  // Using the correct base ensures built asset URLs resolve properly.
+  base: '/ZainteaTracker/',
 })
